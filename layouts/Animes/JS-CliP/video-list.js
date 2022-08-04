3c9fc7dca64308baae4de0029070247aab98bda9
CAKLO <109625411+CAKLO@users.noreply.github.com>
2022-08-04 08:45:32 -0600

    Add files via upload

---
+++ layouts/Animes/JS-CliP/video-list.js
@@ -0,0 +1,71 @@
+let allVideos = [{
+        name: "Capitulo 1",
+        src: "https://mega.nz/embed/afZnQTBK#AO6PUwDeuz3Ydh6x2ggquABudwcAgR8C2Mfi79ovKhQ",
+        id: "vid_1"
+    },
+    {
+        name: "Capitulo 2",
+        src: "https://mega.nz/embed/nK5UiS7a#ABopsQDzXNbTd3aYmQnj9wBrtgIAPaIa0zxvIpkJ6g4",
+        id: "vid_2"
+    },
+    {
+        name: "Capitulo 3",
+        src: "https://mega.nz/embed/2SJCHSoI#ALCQcQCYG_44qAiK1oYqegAkNY0AYoniOE1yvdY9lys",
+        id: "vid_3"
+    },
+    {
+        name: "Capitulo 4 ",
+        src: "https://mega.nz/embed/eSIQ2bgB#ALY9KgD0sx4GE3iARDGVMgCY2WYA0SFLBqzbcEQDb_o",
+        id: "vid_4"
+    },
+    {
+        name: "Capitulo 5",
+        src: "https://mega.nz/embed/PThFgTyJ#ALJ4tQBRrvlURpX3YHAVqgCjuC8AOnx7VFS-62BDZhk",
+        id: "vid_5"
+    },
+    {
+        name: "Capitulo 6",
+        src: "https://mega.nz/embed/LOAAiQ5Z#AOGLKgAh23RqoYI7b3bq0ABFjTkAAxQ7ajd6hG_2Sc4",
+        id: "vid_6"
+    },
+    {
+        name: "Capitulo 7",
+        src: "https://mega.nz/embed/vL5x0QBD#AGB0qQBk9_vwbcDSnGRd0wBY8ccAImY18OqTNZzHlBo",
+        id: "vid_7"
+    },
+    {
+        name: "capitulo 8",
+        src: "https://mega.nz/embed/mCxXDLTQ#ADQjEQDuAL0hX2bf69I5VADfkjsAnv29Ie3epOsFsfc",
+        id: "vid_8"
+    },
+    {
+        name: "capitulo 9",
+        src: "https://mega.nz/embed/WbwBAJLK#AGvsZwDjPcIECvqa5-XHjACOEf8AgHXeBF4G6ueazYU",
+        id: "vid_9"
+    },
+    {
+        name: "Capitulo 10",
+        src: "https://mega.nz/embed/HXBBGICI#AKadMQCsCybshfi7_P03dQCkvLcALkQc7IUOWvzJ-l4",
+        id: "vid_10"
+    },
+    {
+        name: "Capitulo 11",
+        src: "https://mega.nz/embed/XKpnAYRC#ADUxhQByjIJ_BJsNCldTeQBrW6YAbU75fyCTFAq3XZg",
+        id: "vid_11"
+    },
+    {
+        name: "Capitulo 12",
+        src: "https://mega.nz/embed/LV4lDQyA#u3g9kyqdmvmPSAgLWt_IeKSZEfopYWUi64aX6i_e2Ro",
+        id: "vid_12"
+    },
+    {
+        name: "Capitulo 13",
+        src: "https://mega.nz/embed/uCARDA5R#AMyVdABbZGj8gk3cVpc0wQAGa2QAsNwf_KYFE1aXIrw",
+        id: "vid_11"
+    },
+    {
+        name: "Capitulo 14",
+        src: "https://mega.nz/embed/PaYwiKjQ#AHdPqwAF6QlQMEMWDz1AOgA82TQAIcv8UBuZLA__Wg0",
+        id: "vid_12"
+    }
+]
